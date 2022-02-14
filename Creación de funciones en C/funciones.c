86fb38fb1a53d38a48f9336de7589cd5afda2b5d
ACLXRD <andres.lopez01@correo.usa.edu.co>
2022-02-13 21:15:08 -0500

    WIP:05: Creating fuctions in C
    The interface and the functions working correctly. Fuctions to
    basic need to imporve them.
    
    Author: Andrés C. López

---
+++ Creación de funciones en C/funciones.c
@@ -0,0 +1,20 @@
+/*
+    Fecha: 10/02/2022
+    Autor: Andrés C. López
+    Materia: Computación Paralela y Distribuida.
+    Tema: Programación de Funciones en C
+    Descripción: Módulo de funciones
+*/
+
+/*Se presenta la cabecera del módulo*/
+#include "funciones.h"
+
+/*
+    @breif sumar: Suma dos numeros dados
+    @param a: Primer valor para sumar
+    @param b: Segund valor para sumar
+    @return: La suma de @param a y @param b
+*/
+int sumar (int a, int b){;
+    return a + b;
+}
