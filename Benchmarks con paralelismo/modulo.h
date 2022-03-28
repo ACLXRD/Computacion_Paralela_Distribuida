d711581518a9a937396959feb9dbbf7103163fda
ACLXRD <andres.lopez01@correo.usa.edu.co>
2022-03-27 23:21:37 -0500

    WIP:05 Adding the fuction MMmulti to the header file
    Author: Andrés C. López

--- Benchmarks con paralelismo/modulo.h
+++ Benchmarks con paralelismo/modulo.h
@@ -10,7 +10,6 @@
 #ifndef MODULO_C_H_INCLUDE
 #define MODULO_C_H_INCLUDE
 
-
 void sampleStart();
 void sampleEnd();
 double randNumber();
@@ -21,5 +20,6 @@ void matrixMultiplyMM1f(int size, double *Ma, double *Mb, double *Mr);
 double ** memReserve (int size);
 void initMatrix_DoublePointers (double **MA, double **MB, double **MC, int size);
 void printMatrix_DoublePointers (double **M, int size);
+void *multMM(void *arg);
 
 #endif
